e6f1a8edad980f09d33fa96f0e0cd8a628f9ab98
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-16 13:14:57 -0400

    L4 Change

--- PM_L4/src/main.cpp
+++ PM_L4/src/main.cpp
@@ -57,18 +57,4 @@ delay(L);
 digitalWrite(led3, LOW);
 delay(L);
 
-}int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
