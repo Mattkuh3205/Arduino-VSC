697dac3b5953b20877983014178411d33008b6b0
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-21 23:38:26 -0400

    PM_L12

--- PM_L12/src/main.cpp
+++ PM_L12/src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
 
-
+int vout = A0;
+int readVal;
+int v2;
+int del =500;
 
 void setup() {
-  // put your setup code here, to run once:
-  
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readVal=analogRead(vout);
+  v2 = (5./1023.)*readVal;
+
+  Serial.println(v2);
+  delay(del);
+  
+  
+
 }
 
