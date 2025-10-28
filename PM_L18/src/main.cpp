e8d74fdbf7b808cce93f52b5f08bd8ee1411b49c
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-28 16:16:45 -0400

    PM_L18

---
+++ PM_L18/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+//L18: Reading numbers from the serial monitor
+
+int myNumber;
+int led = 8;
+
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+
+ 
+
+ 
+
+
+}
+
