c779e89e2a1411f345fb7e656f1cea3b5f8e88dd
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-28 15:49:56 -0400

    PM_L17

--- PM_L17/src/main.cpp
+++ PM_L17/src/main.cpp
@@ -1,15 +1,49 @@
 #include <Arduino.h>
 
-//PM_L17: Fun with for loops
+//PM_L17: While loops
+
+int x; //counter
+
+int led = 7;
+int potPin = A0;
+int readVal;
+int vOut;
+int Lout;
+int PulseWM;
 
 void setup() {
+Serial.begin(9600);
 
-
-
+pinMode(led, OUTPUT);
+pinMode(potPin, INPUT);
 }
 
 void loop() {
 
+  readVal = analogRead(potPin); //serial 0 -1= 1023
 
+  vOut = (5./1023.)*readVal; //voltage = 5/1023 * readVal (0-1023) 1023 * 5/1023 = 5 volts
+
+  //Lout = (255./1023.)*readVal;
+
+ // analogWrite(led, Lout);
+  
+  //Serial.println(vOut); //voltage
+
+  Serial.println(readVal); //serial value
+      delay(1000);
+
+
+  while(readVal >1000){
+    digitalWrite(led, HIGH);
+    readVal = analogRead(potPin); //serial 0 -1= 1023
+    Serial.println(readVal);
+    delay(1000);
+  }
+
+  digitalWrite(led, LOW);
+  
 
 }
+
+ 
