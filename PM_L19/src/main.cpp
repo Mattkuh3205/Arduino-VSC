df3d8f30c97de3e83f02c5fec45a4aede9b5929a
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-29 15:41:07 -0400

    PM 19

--- PM_L19/src/main.cpp
+++ PM_L19/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//PM L19: Reading strings from the serial monitor
+
+String myName;
+String msg ="What is your name? ";
+String msg2 = "Hello ";
+String msg3 = ", Welcome to Arduino";
+
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//ask
+Serial.println(msg);
+
+//wait
+while (Serial.available()==0){
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+//read
+myName = Serial.readString();
+Serial.print(msg2);
+Serial.print(myName);
+
+Serial.println(msg3);
+
 }
+
