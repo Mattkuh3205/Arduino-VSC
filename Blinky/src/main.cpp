b011e80cee75ce62dcf666f8c6e6859cffab8217
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-29 14:40:18 -0400

    General commit

---
+++ Blinky/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+const int buttonPin = 8;  // the number of the pushbutton pin
+const int ledPin = 13;    // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+digitalWrite(ledPin, LOW);
+
+}
