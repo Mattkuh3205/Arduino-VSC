33c880ace7899ec37de48781eb3abdbc96cde99c
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-30 17:22:37 -0400

    PM_L19

--- PM_L19/src/main.cpp
+++ PM_L19/src/main.cpp
@@ -6,6 +6,8 @@ String myName;
 String msg ="What is your name? ";
 String msg2 = "Hello ";
 String msg3 = ", Welcome to Arduino";
+String msgL ="What led do you want to turn on?";
+String myLED;
 
 int led1 = 8;
 int led2 = 9;
@@ -20,7 +22,7 @@ Serial.begin(9600);
 }
 
 void loop() {
-//ask
+/* //ask
 Serial.println(msg);
 
 //wait
@@ -34,6 +36,53 @@ Serial.print(msg2);
 Serial.print(myName);
 
 Serial.println(msg3);
+ */
+
+
+ Serial.println(msgL);
+
+//wait
+while (Serial.available()==0){
+
+}
+
+//read
+myLED = Serial.readString();
+
+if(myLED =="red"){
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+
+}
+
+if(myLED =="green"){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+  
+
+
+}
+
+
+if(myLED =="blue"){
+   digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+
+
+    
+
+}
+
+if(myLED == "x" || myLED=="X"){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+}
+
+
 
 }
 
