0924661dd7ec2f149b76408594d821d7beaa261f
Matthew Kuhns <mattkuh32@gmail.com>
2025-11-01 19:39:07 -0400

    PM_L20

--- PM_L20/src/main.cpp
+++ PM_L20/src/main.cpp
@@ -1,13 +1,55 @@
 #include <Arduino.h>
 
 //Lesson 20, understanding RGB LED's
+int R = 8;
+int G = 9;
+int B = 10;
 
 void setup() {
+pinMode(R, OUTPUT);
+pinMode(G, OUTPUT);
+pinMode(B, OUTPUT);
 
+Serial.begin(9600);
 }
 
 void loop() {
 
+    Serial.println("Which led do you want to turn on?");
 
+    while(Serial.available()==0){
+
+    }
+    String input;
+
+    input = Serial.readString();
+
+    if(input == "red"){
+        digitalWrite(R, HIGH);
+        digitalWrite(G, LOW);
+        digitalWrite(B, LOW);
+    }
+    if(input == "green"){
+        digitalWrite(R, LOW);
+        digitalWrite(G, HIGH);
+        digitalWrite(B, LOW);
+    }
+    if(input == "blue"){
+        digitalWrite(R, LOW);
+        digitalWrite(G, LOW);
+        digitalWrite(B, HIGH);
+    }
+
+    if(input == "off"){
+          digitalWrite(R, LOW);
+        digitalWrite(G, LOW);
+        digitalWrite(B, LOW);
+    }
+
+    if(input == "orange"){
+        analogWrite(R, 243);
+        analogWrite(G, 114);
+        analogWrite(B, 32);
+    }
 }
 
